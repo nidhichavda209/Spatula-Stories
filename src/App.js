b4bb482476c902ac4db4ef4c39c5bf9bee61af91
ArthSonani <arthsonani@gmail.com>
2025-03-30 01:56:42 +0530

    cuisine card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
@@ -22,30 +22,3 @@ const App = () => {
 }
 
 export default App
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 1d6a0dd (Initialize project using Create React App)
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-
-=======
 import reportWebVitals from './reportWebVitals';
->>>>>>> 1d6a0dd (Initialize project using Create React App)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,11 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-<<<<<<< HEAD
-
-=======
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> 1d6a0dd (Initialize project using Create React App)
